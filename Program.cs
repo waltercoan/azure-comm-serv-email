862a7c3be518c247c873dcb3c0346f223cb8c8fc
Walter Silvestre Coan <walter.coan@gmail.com>
2024-05-28 09:32:05 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using Azure;
+using Azure.Communication.Email;
+using Azure.Identity;
+
+//Endpoint do Communication Service (Menu Settings - Keys)
+EmailClient emailClient = new EmailClient(new Uri("https://<NOME-INSTANCIA-COMMSERVER>.communication.azure.com/"),new DefaultAzureCredential());
+
+//MailFrom addresses do Email Communication Services Domain
+var sender = "donotreply@<GUID-DOMAIN-AZURE-COMMSERVEREMAIL>.azurecomm.net";
+
+var toRecipients = new List<EmailAddress>
+{
+  new EmailAddress("..."),
+  new EmailAddress("..."),
+};
+
+EmailRecipients emailRecipients = new EmailRecipients(toRecipients);
+
+var emailContent = new EmailContent("Welcome to Azure Communication Service Email APIs.")
+{
+    PlainText = "This email message is sent from Azure Communication Service Email.",
+    Html = "<html><body><h1>Quick send email test</h1><br/><h4>This email message is sent from Azure Communication Service Email.</h4><p>This mail was sent using .NET SDK!!</p></body></html>"
+};
+var emailMessage = new EmailMessage(
+    senderAddress: sender,
+    emailRecipients,
+    emailContent);
+
+try
+{
+    Console.WriteLine("Sending email...");
+    EmailSendOperation emailSendOperation = await emailClient.SendAsync(Azure.WaitUntil.Completed,emailMessage);
+    
+    EmailSendResult statusMonitor = emailSendOperation.Value;
+    
+    Console.WriteLine($"Email Sent. Status = {emailSendOperation.Value.Status}");
+
+    /// Get the OperationId so that it can be used for tracking the message for troubleshooting
+    string operationId = emailSendOperation.Id;
+    Console.WriteLine($"Email operation id = {operationId}");
+}
+catch (RequestFailedException ex)
+{
+    /// OperationID is contained in the exception message and can be used for troubleshooting purposes
+    Console.WriteLine($"Email send operation failed with error code: {ex.ErrorCode}, message: {ex.Message}");
+}
---
+++ Readme.md
@@ -0,0 +1,10 @@
+# Azure Communication Service - Envio de E-mail
+
+Exemplo de como criar enviar um e-mail utilizando o Azure Communication Service em C#
+
+[![My Skills](https://skillicons.dev/icons?i=azure,dotnet)](https://skillicons.dev)
+
+[Documentação envio e-mail](https://learn.microsoft.com/en-us/azure/communication-services/quickstarts/email/send-email?wt.mc_id=AZ-MVP-5003638)
+
+## Deploy do serviço no Azure
+[![Deploy to Azure](https://aka.ms/deploytoazurebutton)](https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2Fwaltercoan%2Fazure-comm-serv-email%2Fmain%2Fazuredeploy.json)
